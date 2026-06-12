ccf3cc90c5e18c3dbe5cba7890f1089b041cab62
sooubh <sourabh3527@gmail.com>
2026-06-12 19:18:07 +0530

    fix: resolve AGP namespace requirements in Gradle subprojects build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,39 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureProject = {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            val android = extensions.findByName("android")
+            if (android != null) {
+                try {
+                    val getNamespace = android.javaClass.getMethod("getNamespace")
+                    val currentNamespace = getNamespace.invoke(android) as? String
+                    if (currentNamespace.isNullOrEmpty()) {
+                        val setNamespace = android.javaClass.getMethod("setNamespace", java.lang.String::class.java)
+                        val ns = if (project.group.toString().isEmpty() || project.group.toString() == "compass" || project.group.toString() == "sbi") {
+                            "com.example.${project.name.replace("-", "_").replace(".", "_")}"
+                        } else {
+                            project.group.toString()
+                        }
+                        setNamespace.invoke(android, ns)
+                    }
+                } catch (e: Exception) {
+                    // Ignore errors if setNamespace is not supported
+                }
+            }
+        }
+    }
+
+    if (state.executed) {
+        configureProject()
+    } else {
+        afterEvaluate {
+            configureProject()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
